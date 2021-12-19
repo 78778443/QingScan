5140f8511a664ad64b2806a43037d129e2263f47
婷婷的橙子 <2350906567@qq.com>
2021-12-19 19:04:04 +0800

    修复主机列表报错2

--- code/view/app/details.php
+++ code/view/app/details.php
@@ -291,7 +291,7 @@ $typeArr = [
                                 <td><?php echo $value['area'] ?></td>
                                 <td><?php echo $value['hydra_scan_time'] ?></td>
                                 <td><?php echo $value['port_scan_time'] ?></td>
-                                <td><?php echo $value['target'] ?></td>
+                                <td><?php echo $value['ip_scan_time'] ?></td>
                                 <td><?php echo $value['is_delete'] ?></td>
                                 <td><?php echo $value['user_id'] ?></td>
                             </tr>
