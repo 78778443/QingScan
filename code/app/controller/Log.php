789c04d51139412c5e0e622e7914448518821524
linyuanjia <11072162@qq.com>
2021-12-28 23:18:52 +0800

    添加日志搜索功能

--- code/app/controller/Log.php
+++ code/app/controller/Log.php
@@ -6,12 +6,19 @@ namespace app\controller;
 
 use think\facade\Db;
 use think\facade\View;
+use think\Request;
 
 class Log extends Common
 {
-    public function index()
+    public function index(Request $request)
     {
-        $list = Db::table('log')->order("id", 'desc')->paginate(20);
+        $pageSize = 20;
+        $search = $request->param('search');
+        $where = [];
+        if ($search) {
+            $where[] = ['content','like',"%{$search}%"];
+        }
+        $list = Db::table('log')->where($where)->order("id", 'desc')->paginate(['list_rows' => $pageSize, 'query' => request()->param()]);
         $data['list'] = $list->items();
         $data['page'] = $list->render();
         return View::fetch('index', $data);
