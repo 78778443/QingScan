734163ecc4123504d1ba6e1d8b187f5c5d4bd26e
tangqingsong <78778443@qq.com>
2021-12-29 15:37:54 +0800

    先关闭这行代码

---
+++ code/config/task.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'TASK_SCAN' => true
+];
---
+++ code/view/config/update.php
@@ -0,0 +1,11 @@
+{include file='public/head' /}
+<div class="row" style="height: 50px"></div>
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6 tuchu">
+        <code>{$info}</code>
+    </div>
+    <div class="col-md-3"></div>
+</div>
+<div class="row" style="height: 50px"></div>
+{include file='public/footer' /}
