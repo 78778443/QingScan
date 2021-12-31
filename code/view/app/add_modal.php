953237c32810ad8c671ae34c1439f692a2c28af8
daxia <78778443@qq.com>
2021-12-31 21:08:20 +0800

    界面文案优化

--- code/view/app/add_modal.php
+++ code/view/app/add_modal.php
@@ -2,14 +2,14 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">添加</h5>
+                <h5 class="modal-title" id="exampleModalLabel">添加黑盒检测目标</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form method="post" action="<?php echo url('app/_add') ?>">
                 <input type="hidden" name="username" class="form-control" placeholder="账号">
                 <input type="hidden" name="password" class="form-control" placeholder="URL">
                 <div class="modal-body">
-                    <h3>基本信息</h3>
+                    <p style="color:red">注意:QingScan 仅授权你在遵循《网络安全法》的前提下使用，请不要擅自对未获得授权的目标进行安全测试！！！</p>
                     <div class="mb-3">
                         <label class="form-label">应用名称</label>
                         <input type="text" name="name" class="form-control" placeholder="应用名称" required>
@@ -18,14 +18,14 @@
                         <label class="form-label">URL地址</label>
                         <input type="url" name="url" class="form-control" placeholder="URL" required>
                     </div>
-<!--                    <div class="mb-3">-->
-<!--                        <label class="form-label">账号</label>-->
-<!--                        <input type="text" name="username" class="form-control" placeholder="账号">-->
-<!--                    </div>-->
-<!--                    <div class="mb-3">-->
-<!--                        <label class="form-label">密码</label>-->
-<!--                        <input type="text" name="password" class="form-control" placeholder="URL">-->
-<!--                    </div>-->
+                    <!--                    <div class="mb-3">-->
+                    <!--                        <label class="form-label">账号</label>-->
+                    <!--                        <input type="text" name="username" class="form-control" placeholder="账号">-->
+                    <!--                    </div>-->
+                    <!--                    <div class="mb-3">-->
+                    <!--                        <label class="form-label">密码</label>-->
+                    <!--                        <input type="text" name="password" class="form-control" placeholder="URL">-->
+                    <!--                    </div>-->
                     <div class="mb-3">
                         <label class="form-label">是否xary扫描</label>
                         <select name="is_xray" class="form-select" aria-label="Default select">
