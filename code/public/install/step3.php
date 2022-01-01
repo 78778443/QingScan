86d00199bb19d9d343b8a6306e71e354f87f9d5c
linyuanjia <11072162@qq.com>
2022-01-01 23:28:45 +0800

    测试

--- code/public/install/step3.php
+++ code/public/install/step3.php
@@ -103,7 +103,7 @@ function addOldData()
             }
             $filename = explode('/',$filepath);
             $update_content = $filename[count($filename) - 1];
-            file_put_contents('update.lock',$update_content);
+            var_dump(file_put_contents('update.lock',$update_content));
         }
     }
 }
