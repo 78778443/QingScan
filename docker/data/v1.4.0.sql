3490695a62d88e9b24cb06a23a4786928ffac749
linyuanjia <11072162@qq.com>
2022-01-23 15:38:53 +0800

    1

---
+++ docker/data/v1.4.0.sql
@@ -0,0 +1,3 @@
+INSERT INTO `QingScan`.`auth_rule` (`href`, `title`, `is_delete`, `is_open_auth`, `pid`, `sort`, `created_at`, `menu_status`, `update_time`, `level`, `delete_time`, `icon_url`) VALUES ('github_keyword_monitor_notice/index', 'github关键词监控结果', 0, 1, 35, 8, 1642852575, 1, 0, 2, 0, '');
+
+INSERT INTO `QingScan`.`auth_rule` (`href`, `title`, `is_delete`, `is_open_auth`, `pid`, `sort`, `created_at`, `menu_status`, `update_time`, `level`, `delete_time`, `icon_url`) VALUES ('backup/index', '数据备份', 0, 1, 23, 8, 1642854435, 1, 0, 2, 0, '');
