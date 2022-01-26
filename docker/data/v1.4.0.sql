b9deefbd2b39430bc5225803cd63cfd7f7ea5650
linyuanjia <11072162@qq.com>
2022-01-26 15:04:16 +0800

    更新sql文件

--- docker/data/v1.4.0.sql
+++ docker/data/v1.4.0.sql
@@ -2,4 +2,18 @@ INSERT INTO `QingScan`.`auth_rule` (`href`, `title`, `is_delete`, `is_open_auth`
 
 INSERT INTO `QingScan`.`auth_rule` (`href`, `title`, `is_delete`, `is_open_auth`, `pid`, `sort`, `created_at`, `menu_status`, `update_time`, `level`, `delete_time`, `icon_url`) VALUES ('backup/index', '数据备份', 0, 1, 23, 8, 1642854435, 1, 0, 2, 0, '');
 
+INSERT INTO `QingScan`.`auth_rule` (`href`, `title`, `is_delete`, `is_open_auth`, `pid`, `sort`, `created_at`, `menu_status`, `update_time`, `level`, `delete_time`, `icon_url`) VALUES ('user_log/index', '登录日志', 0, 1, 23, 9, 1643018839, 1, 0, 2, 0, '');
+
 ALTER TABLE `QingScan`.`github_keyword_monitor_notice` ADD COLUMN `app_id` int(11) NOT NULL DEFAULT 0 AFTER `app_id`;
+
+ALTER TABLE `QingScan`.`github_keyword_monitor` ADD COLUMN `app_id` int(11) NOT NULL DEFAULT 0 AFTER `app_id`;
+
+CREATE TABLE `user_log` (
+                            `id` int(10) NOT NULL AUTO_INCREMENT,
+                            `username` char(30) NOT NULL DEFAULT '',
+                            `create_time` datetime NOT NULL,
+                            `content` varchar(255) NOT NULL DEFAULT '' COMMENT '详情',
+                            `type` varchar(50) NOT NULL DEFAULT '' COMMENT '操作类型',
+                            `ip` varchar(50) NOT NULL DEFAULT '',
+                            PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4;
