a2cd2bb2c8fc1eb56b01d2ca6517028af65434f6
linyuanjia <11072162@qq.com>
2022-02-10 19:00:07 +0800

    新增暂停扫描配置sql

--- docker/data/v1.4.0.sql
+++ docker/data/v1.4.0.sql
@@ -19,3 +19,5 @@ CREATE TABLE `user_log` (
 ) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `QingScan`.`auth_rule` (`href`, `title`, `is_delete`, `is_open_auth`, `pid`, `sort`, `created_at`, `menu_status`, `update_time`, `level`, `delete_time`, `icon_url`) VALUES ('zhiwen/index', '指纹列表', 0, 1, 35, 11, 1643338608, 1, 0, 2, 0, '');
+
+INSERT INTO `QingScan`.`system_config` (`name`, `key`, `value`, `is_delete`) VALUES ('暂停扫描', 'maxProcesses', '1', 0);
