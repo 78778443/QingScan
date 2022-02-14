76888de5627c8308e54f24a33566969292def86a
linyuanjia <11072162@qq.com>
2022-02-14 22:31:10 +0800

    1

--- code/public/index.php
+++ code/public/index.php
@@ -12,8 +12,9 @@
 namespace think;
 
 header('Access-Control-Allow-Origin:*');
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-header('Access-Control-Allow-Headers:Origin,Content-Type,Accept,token,X-Requested-With,device');
+header('Access-Control-Allow-Headers: content-type,token');
+
+header('Content-Type: text/html; charset=utf-8');
 
 if (!file_exists(__DIR__."/install/install.lock")) {
     header("location:/install/step1.php");exit();
