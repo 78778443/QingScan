625ca77a4e0f5b2e7e3b4b7f9c0eb7c179c1c9b3
linyuanjia <11072162@qq.com>
2022-02-14 22:55:42 +0800

    1

--- code/app/controller/DeskIndex.php
+++ code/app/controller/DeskIndex.php
@@ -17,7 +17,8 @@ class DeskIndex extends BaseController
 
     public function initialize()
     {
-        $token = getParam('token');
+        //$token = getParam('token');
+        $token = '1ca4725c34758183af3fd1f723f07a31';
         $this->userId = Db::name('user')->where('token', $token)->value('id');
         $this->auth_group_id = Db::name('user')->where('token', $token)->value('auth_group_id');
 
