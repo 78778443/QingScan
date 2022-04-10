723610785b7b5632b6508756f5bdc773a304eeac
daxia <78778443@qq.com>
2022-04-10 21:35:57 +0800

    自动删除代码

--- code/app/model/PluginModel.php
+++ code/app/model/PluginModel.php
@@ -48,6 +48,36 @@ class PluginModel extends BaseModel
         }
     }
 
+    public static function deleteDir()
+    {
+        $codeCheck = "/data/codeCheck";
+        //1. 获取目录下的文件列表
+        $resource = opendir($codeCheck);
+        while ($file = readdir($resource)) {
+            $dirName = "{$codeCheck}/{$file}";
+            //2. 获取文件的创建时间
+            $ctime = filectime($dirName);
+
+            //获取磁盘空间,根据剩余空间，决定代码保存时间
+            $cmd = "df -h | grep 'G' | head -n 1 | awk '{print $4}'";
+            $size = intval(exec($cmd));
+            //如果空间大于200G,那么保留72小时,否则48小时
+            $hour = ($size > 200) ? 72 : 48;
+            //如果空间小于60G,那么保留24小时
+            $hour = ($size < 60) ? 24 : $hour;
+            //如果空间小于20G,那么保留2小时
+            $hour = ($size < 20) ? 2 : $hour;
+
+            echo "空间剩余 {$size} G 保留时间为 {$hour} 小时" . PHP_EOL;
+
+            //3. 如果时间超过24小时
+            if ($ctime < (time() - $hour * 3600)) {
+                //4. 删除此文件
+                unlink($dirName);
+            }
+        }
+    }
+
     public static function safe()
     {
         // 死循环不断监听任务是不是挂了
@@ -133,11 +163,16 @@ class PluginModel extends BaseModel
                     if (!$content) {
                         addlog(["自定义工具扫描结果文件内容为空", $info, $v]);
                     }
-                    @unlink($result_path);
-                } elseif(strpos($info['result_type'], 'csv')){
-                    $content = assoc_getcsv($filename);
-                    if (!$content) {
-                        addlog(["自定义工具扫描结果文件内容为空",$info,$v]);
+                } elseif (strpos($cmd, '.txt')) {
+                    $result_path = '';
+                    $file = fopen($result_path, "r");
+                    //检测指正是否到达文件的未端
+                    $data = [];
+                    while (!feof($file)) {
+                        $result = fgets($file);
+                        if ($result) {
+                            $data[] = $result;
+                        }
                     }
                     @unlink($result_path);
                 } else {
