715dcfa471b2b55b2f8c7d7f7c3fff99d259435e
linyuanjia <qq11072162@gmail.com>
2022-05-11 16:06:29 +0800

    修改首页数据统计显示

--- code/app/controller/Index.php
+++ code/app/controller/Index.php
@@ -53,6 +53,7 @@ class Index extends Common
         $codeCount = Db::table('code')->count();
         $semgrepCount = Db::table('semgrep')->count();
         $fortifyCount = Db::table('fortify')->count();
+        $mobsfscanCount = Db::table('mobsfscan')->count();
         $phpCount = Db::table('code_composer')->count();
         $pythonCount = Db::table('code_python')->count();
         $javaCount = Db::table('code_java')->count();
@@ -96,6 +97,7 @@ class Index extends Common
                 "subInfo" => [
                     ["name" => "fortify", "value" => $fortifyCount, "href" => url('code/bug_list')],
                     ["name" => "semgrep", "value" => $semgrepCount, "href" => url('code/semgrep_list')],
+                    ["name" => "mobsfscan", "value" => $mobsfscanCount, "href" => url('mobsfscan/index')],
                     ["name" => "webshell", "value" => $hemaCount, "href" => url('code_webshell/index')],
                     ["name" => "php", "value" => $phpCount, "href" => url('code_composer/index')],
                     ["name" => "python", "value" => $pythonCount, "href" => url('code_python/index')],
