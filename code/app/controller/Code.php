c5d5f814f3a65f573c4ddd680f0d835bddfa611d
linyuanjia <qq11072162@gmail.com>
2022-05-16 17:59:17 +0800

    修复白盒审计报错

--- code/app/controller/Code.php
+++ code/app/controller/Code.php
@@ -859,12 +859,12 @@ class Code extends Common
                 $this->error($e->getMessage());
             }
         } else {
-            /*$data['tools_list'] = [
+            $data['tools_list'] = [
                 'semgrep'=>'kunlun',
                 'kunlun'=>'kunlun',
                 'fortify'=>'fortify',
                 'webshell'=>'webshell',
-            ];*/
+            ];
             return View::fetch('add_file', $data);
         }
     }
